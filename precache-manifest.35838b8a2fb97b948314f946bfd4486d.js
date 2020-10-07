49e44dbd51fd83668e9c16721ff0afad0689e655
a <a@MacBook-Air-a.local>
2020-10-08 02:47:39 +0300

    Updates

---
+++ precache-manifest.35838b8a2fb97b948314f946bfd4486d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d961c9fe40f24dc4aad88bb3de09e780",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "e14a415b75426fbd92d8",
+    "url": "/react-app/static/css/main.5e46de05.chunk.css"
+  },
+  {
+    "revision": "ca0796b1ca21b341072c",
+    "url": "/react-app/static/js/2.8f22454b.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/react-app/static/js/2.8f22454b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14a415b75426fbd92d8",
+    "url": "/react-app/static/js/main.b2319a3e.chunk.js"
+  },
+  {
+    "revision": "759b0e3654343618c603",
+    "url": "/react-app/static/js/runtime-main.c5c9f457.js"
+  },
+  {
+    "revision": "4e76d415f37ddd065a0f08332074bbc0",
+    "url": "/react-app/static/media/user.4e76d415.png"
+  }
+]);
